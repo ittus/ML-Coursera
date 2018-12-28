8c6fe1562c0ebd4cf8383b0295765735db253e27
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-12-28 23:30:25 +0900

    Log accuracy

--- ML-Tensorflow-Javascript/MLKits/plinko/score.js
+++ ML-Tensorflow-Javascript/MLKits/plinko/score.js
@@ -1,5 +1,4 @@
 const outputs = []
-const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -7,8 +6,21 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-	.map(row => [distance(row[0]), row[3]])
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+
+  const accuracy = _.chain(testSet)
+   .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3])
+   .size()
+   .divide(testSetSize)
+   .value();
+   console.log('Accuracy: ', accuracy);
+
+}
+
+function knn (data, point) {
+  return _.chain(data)
+	.map(row => [distance(row[0], point), row[3]])
 	.sortBy(row => row[0])
 	.slice(0, k)
   .countBy(row => row[1])
@@ -18,9 +30,15 @@ function runAnalysis() {
   .first()
   .parseInt()
   .value()
-  console.log('Your point will probably will fall into ',bucket)
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset (data, testCount) {
+  const shuffled = _.shuffle(data);
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+  return [testSet, trainingSet];
 }
