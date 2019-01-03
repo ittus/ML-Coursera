8ddaf2cf6e27d557f0b5df8edc3f3252e163811f
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-01-04 00:26:19 +0900

    First implementation of gradient descent

--- ML-Tensorflow-Javascript/MLKits/regressions/index.js
+++ ML-Tensorflow-Javascript/MLKits/regressions/index.js
@@ -1,7 +1,7 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-
 const loadCSV = require('./load-csv');
+const LinearRegression = require('./linear-regression');
 
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   shuffle: true,
@@ -10,4 +10,10 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   labelColumns: ['mpg']
 });
 
-console.log(labels)
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.00001,
+  iterations: 100
+});
+
+regression.train()
+console.log('New m: ', regression.m, 'New b: ', regression.b)
