7d72fb259586017667889d22e447d52e2ef11af0
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-01-04 19:07:50 +0900

    Change to Batch GD

--- ML-Tensorflow-Javascript/MLKits/regressions/linear-regression.js
+++ ML-Tensorflow-Javascript/MLKits/regressions/linear-regression.js
@@ -16,19 +16,35 @@ class LinearRegression {
   }
 
   train () {
+    const batchQuantity = Math.floor(this.features.shape[0] / this.options.batchSize);
+
     for (let i = 0; i < this.options.iterations; i++) {
-      this.gradientDescent();
+      for (let j = 0; j < batchQuantity; j++) {
+        const startIndex = j * this.options.batchSize;
+        const { batchSize } = this.options;
+
+        const featureSlice = this.features.slice(
+          [startIndex, 0],
+          [batchSize, -1]
+        );
+
+        const labelSlice = this.labels.slice(
+          [startIndex, 0],
+          [batchSize, -1]
+        );
+        this.gradientDescent(featureSlice, labelSlice);
+      }
       this.recordMSE();
       this.updateLearningRate();
     }
   }
 
-  gradientDescent () {
-    const currentGuesses = this.features.matMul(this.weights);
-    const differences = currentGuesses.sub(this.labels);
-    const slopes = this.features.transpose()
+  gradientDescent (features, labels) {
+    const currentGuesses = features.matMul(this.weights);
+    const differences = currentGuesses.sub(labels);
+    const slopes = features.transpose()
       .matMul(differences)
-      .div(this.features.shape[0])
+      .div(features.shape[0])
 
     this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
   }
