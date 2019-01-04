f07b9683ea6934212a11ba25cddb683713098fb6
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-01-04 17:09:23 +0900

    Optimize learning rate

--- ML-Tensorflow-Javascript/MLKits/regressions/linear-regression.js
+++ ML-Tensorflow-Javascript/MLKits/regressions/linear-regression.js
@@ -5,18 +5,21 @@ class LinearRegression {
   constructor (features, labels, options) {
     this.features = this.processFeatures(features);
     this.labels = tf.tensor(labels);
+    this.mseHistory = [];
 
     this.options = Object.assign({
       learningRate: 0.1,
       iterations: 1000
     }, options);
 
-    this.weights = tf.zeros([2, 1]);
+    this.weights = tf.zeros([this.features.shape[1], 1]);
   }
 
   train () {
     for (let i = 0; i < this.options.iterations; i++) {
       this.gradientDescent();
+      this.recordMSE();
+      this.updateLearningRate();
     }
   }
 
@@ -68,6 +71,29 @@ class LinearRegression {
     this.variance = variance;
 
     return features.sub(mean).div(variance.pow(0.5));
+  }
+
+  recordMSE () {
+    const mse = this.features
+      .matMul(this.weights)
+      .sub(this.labels)
+      .pow(2)
+      .sum()
+      .div(this.features.shape[0])
+      .get()
+    this.mseHistory.unshift(mse);
+  }
+
+  updateLearningRate () {
+    if (this.mseHistory.length < 2) {
+      return;
+    }
+
+    if (this.mseHistory[0] > this.mseHistory[1]) {
+      this.options.learningRate /= 2;
+    } else {
+      this.options.learningRate *= 1.05;
+    }
   }
 }
 
